9988e88e75d83d1f34cbaa3e378cee2811e837be
ChrisSkyRo <chrisskyro@gmail.com>
2023-03-24 17:03:11 +0200

    Added project

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Spanzuratoare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SentenceToGuess = new System.Windows.Forms.Label();
+            this.GuessedLetters = new System.Windows.Forms.Label();
+            this.PickLetter = new System.Windows.Forms.Button();
+            this.LetterInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SentenceToGuess
+            // 
+            this.SentenceToGuess.AutoSize = true;
+            this.SentenceToGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SentenceToGuess.Location = new System.Drawing.Point(12, 36);
+            this.SentenceToGuess.Name = "SentenceToGuess";
+            this.SentenceToGuess.Size = new System.Drawing.Size(70, 25);
+            this.SentenceToGuess.TabIndex = 0;
+            this.SentenceToGuess.Text = "label1";
+            // 
+            // GuessedLetters
+            // 
+            this.GuessedLetters.AutoSize = true;
+            this.GuessedLetters.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GuessedLetters.ForeColor = System.Drawing.Color.Maroon;
+            this.GuessedLetters.Location = new System.Drawing.Point(12, 185);
+            this.GuessedLetters.Name = "GuessedLetters";
+            this.GuessedLetters.Size = new System.Drawing.Size(99, 20);
+            this.GuessedLetters.TabIndex = 1;
+            this.GuessedLetters.Text = "Litere alese: ";
+            // 
+            // PickLetter
+            // 
+            this.PickLetter.Location = new System.Drawing.Point(139, 128);
+            this.PickLetter.Name = "PickLetter";
+            this.PickLetter.Size = new System.Drawing.Size(69, 23);
+            this.PickLetter.TabIndex = 2;
+            this.PickLetter.Text = "Alege litera";
+            this.PickLetter.UseVisualStyleBackColor = true;
+            this.PickLetter.Click += new System.EventHandler(this.PickLetter_Click);
+            // 
+            // LetterInput
+            // 
+            this.LetterInput.Location = new System.Drawing.Point(16, 128);
+            this.LetterInput.Name = "LetterInput";
+            this.LetterInput.Size = new System.Drawing.Size(100, 20);
+            this.LetterInput.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 312);
+            this.Controls.Add(this.LetterInput);
+            this.Controls.Add(this.PickLetter);
+            this.Controls.Add(this.GuessedLetters);
+            this.Controls.Add(this.SentenceToGuess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label SentenceToGuess;
+        private System.Windows.Forms.Label GuessedLetters;
+        private System.Windows.Forms.Button PickLetter;
+        private System.Windows.Forms.TextBox LetterInput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Spanzuratoare
+{
+    public partial class Form1 : Form
+    {
+        private int lives = 6;
+        private string sentence = "C a l i n   e s t e   o r f a n";
+        private bool[] fr = new bool[26];
+
+        private void DrawHangMan()
+        {
+            Pen thick = new Pen(Color.Black, 5);
+            Pen thin = new Pen(Color.Black, 2);
+            Graphics draw = CreateGraphics();
+
+            if (lives == 6)
+            {
+                draw.DrawLine(thick, 350, 50, 350, 200);
+                draw.DrawLine(thick, 325, 200, 375, 200);
+                draw.DrawLine(thick, 350, 50, 400, 50);
+                draw.DrawLine(thin, 400, 50, 400, 100);
+            }
+            else if(lives == 5)
+            {
+                draw.DrawEllipse(thin, 390, 100, 20, 20);
+            }
+            else if(lives == 4)
+            {
+                draw.DrawLine(thick, 400, 120, 400, 150);
+            }
+            else if (lives == 3)
+            {
+                draw.DrawArc(thin, 375, 125, 50, 50, 270, 90);
+            }
+            else if (lives == 2)
+            {
+                draw.DrawArc(thin, 375, 125, 50, 50, 270, -90);
+            }
+            else if (lives == 1)
+            {
+                draw.DrawLine(thin, 400, 145, 375, 175);
+            }
+            else
+            {
+                draw.DrawLine(thin, 400, 145, 425, 175);
+                if (MessageBox.Show("Ai pierdut! Renunti?", "Gheim ovar", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+                else
+                {
+                    Form1 q = new Form1();
+                    this.Hide();
+                    q.Show();
+                }
+            }
+        }
+
+        private bool Solve(char c)
+        {
+            bool ok = false;
+            char[] newText = SentenceToGuess.Text.ToCharArray();
+            for (int i = 0; i < sentence.Length; i++)
+                if (sentence[i] == c || sentence[i] == (char)(c + 32))
+                { 
+                    newText[i] = sentence[i];
+                    ok = true;
+                }
+            SentenceToGuess.Text = new string(newText);
+            if (sentence == SentenceToGuess.Text)
+            {
+                MessageBox.Show("weiner, weiner, chiken dinner");
+                PickLetter.Enabled = false;
+            }
+            return ok;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            SentenceToGuess.Text = "_ _ _ _ _   _ _ _ _   _ _ _ _ _";
+        }
+
+        private void PickLetter_Click(object sender, EventArgs e)
+        {
+            if (LetterInput.Text.Length == 0)
+            {
+                MessageBox.Show("Alege o litera!");
+            }
+            else if (LetterInput.Text.Length > 1)
+            {
+                MessageBox.Show("Alege o SINGURA litera!");
+                LetterInput.Text = "";
+            }
+            else
+            {
+                char c = LetterInput.Text[0];
+                if (c >= 'a' && c <= 'z')
+                    c -= (char)(32);
+                if (c >= 'A' && c <= 'Z')
+                {
+                    if (!fr[c - 'A'])
+                    {
+                        if (!Solve(c))
+                        {
+                            lives--;
+                            DrawHangMan();
+                        }
+                        GuessedLetters.Text += c + ", ";
+                        LetterInput.Text = "";
+                        fr[c - 'A'] = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ai ghicit litera asta deja!");
+                        LetterInput.Text = "";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Litere, orfanule, litere!");
+                    LetterInput.Text = "";
+                }
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if(lives == 6)
+                DrawHangMan();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
